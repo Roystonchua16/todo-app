0f11fd1b9d189a7fbad1aad60919b85ce951bf47
royston <roystonchua16@gmail.com>
2018-04-14 01:13:47 +0800

    commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+var exphbs  = require('express-handlebars');
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+app.get("/home", (req, res) => {
+    res.render("home");
+});
+
+app.get("/css/style.css", (req, res) => {
+    res.sendFile(__dirname + '/css/style.css');
+});
+
+app.listen("3000");
+console.log("localhost:3000");
